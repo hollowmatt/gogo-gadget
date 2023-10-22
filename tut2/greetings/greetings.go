26f5f2061daf9138f4718934207baef983c684d6
Matt Holloway <hollowmatt@gmail.com>
2023-10-22 18:07:54 +0000

    add error handling

--- tut2/greetings/greetings.go
+++ tut2/greetings/greetings.go
@@ -2,7 +2,11 @@ package greetings
 
 import "fmt"
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	message := fmt.Sprintf("Hi, %v.  Welcome!", name)
-	return message
+	return message, nil
 }
